d9c1161accdbef6fc03e51328273f2ffc44c3d84
Lionel Kaneza <lionkaneza@gmail.com>
2023-08-19 14:12:12 +0200

    fix: remove .env file

---
+++ models/user.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  names: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+});
+
+module.exports = mongoose.model("User", userSchema);
